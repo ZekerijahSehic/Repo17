ad14c962b6c2e43e27ba41742be44ea73314f515
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-30 09:21:32 +0200

    DA to Assign Variables

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -2,16 +2,15 @@ const MY_CAR = {
     wheels: 4,
     cylindres: 4,
     size: 2.2
-}
+};
 
 function main(myCar) {
     // Only change code below this line
 
     const { cylindres: carCylindres, size: carSize} = myCar; 
-
     return {carCylindres, carSize};
-    // Only change code above this line
 
+    // Only change code above this line
     
 }
 
